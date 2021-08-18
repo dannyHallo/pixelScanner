b0404062f7ce4e1f3ec8f5814326466a516fe722
dannyHallo <2759370364@qq.com>
2021-08-18 18:50:52 +0800

    fixes

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from PIL import Image
 import pyperclip
 
 
-def process_pic(file):
+def process_pic_3216(file):
     im = Image.open(file, 'r')
     width, height = im.size
-    pic_size = int(width * height / 8)
+    pic_size_local = int(width * height / 8)
     temp_hex = 0x00
     bit_count = 0
     next_line_count = 0
@@ -36,7 +36,7 @@ def process_pic(file):
                 temp_hex <<= 1
     output_list.append('}')
     temp = "".join(output_list)
-    return temp, pic_size
+    return temp, pic_size_local
 
 
 final_list = []
@@ -46,7 +46,7 @@ pic_size = 0
 for file in os.listdir():
     try:
         print(file)
-        pic_data, pic_size = process_pic(file)
+        pic_data, pic_size = process_pic_3216(file)
         final_list.append(pic_data)
         final_list.append(',')
         total_pics += 1
